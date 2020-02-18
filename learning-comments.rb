a2bf0a5ce0dbd828037b569339a8a1ffb0ac5c12
David Wolff <dcwolff218@gmail.com>
2020-02-18 21:51:59 +0000

    Automatically backed up by Learn

---
+++ learning-comments.rb
@@ -0,0 +1,7 @@
+#perform a constant expression evaluation
+3
+#Assign constant 3 to bare-word variable triangle_side
+'triangle_sides'
+#from the three dog night song jeremiah was a bull frog blabblablbalblablablablababalbalbalab
+puts 'joy to the world'
+puts "all the boys and girls"
